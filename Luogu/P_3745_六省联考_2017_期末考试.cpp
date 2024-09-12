f6167367cc7c1976138b7bc6377f5a9a9e4b9e27
ChaomengCFX <2198818239@qq.com>
2024-09-12 18:01:34 +0800

    P3745

--- Luogu/P_3745_六省联考_2017_期末考试.cpp
+++ Luogu/P_3745_六省联考_2017_期末考试.cpp
@@ -4,12 +4,12 @@
 using i64 = long long;
 
 #define TRACE(x) std::cout << "TRACE: " << #x << " = " << (x) << std::endl;
-#define DEBUG(...) std::cerr << "DEBUG: " << __VA_ARGS__ << std::endl;
+#define DEBUG(...) //std::cerr << "DEBUG: " << __VA_ARGS__ << std::endl;
 #define DEBUGV(vec, size) std::cerr << '['; for (int i = 0; i < size; i++) { std::cerr << vec[i]; if (i != size - 1) std::cerr << ", "; } std::cerr << ']' << std::endl;
 
 void solve()
 {
-    int A, B, C, n, m;
+    i64 A, B, C, n, m;
     std::cin >> A >> B >> C >> n >> m;
     std::vector<int> t(n), b(m);
     for (int i = 0; i < n; i++) {
@@ -21,44 +21,53 @@ void solve()
     std::sort(b.begin(), b.end());
 
     auto calMove = [&](int time) -> i64 {
-        int lt = 0, rt = 0;
+        i64 lt = 0, rt = 0;
         if (A < B) {
             for (int i = 0; i < m; i++) {
                 if (b[i] < time) lt += time - b[i];
                 else rt += b[i] - time;
             }
-            int pt = std::min(lt, rt);
-            return 1LL * pt * A + (0LL + rt - pt) * B;
+            i64 pt = std::min(lt, rt);
+            DEBUG("calMove: " << pt * A + (rt - pt) * B);
+            return pt * A + (rt - pt) * B;
         } else {
             for (int i = 0; i < m; i++) {
                 if (b[i] > time) rt += b[i] - time;
             }
-            DEBUG(rt * B);
-            return 1LL * rt * B;
+            DEBUG("calMove: " << rt * B);
+            return rt * B;
         }
     };
+
+    if (C >= 1E16) {
+        std::cout << calMove(*std::min_element(t.begin(), t.end())) << std::endl;
+        return;
+    }
 
     auto calWait = [&](int time) -> i64 {
         i64 sum = 0;
         for (int i = 0; i < n; i++) {
             if (t[i] < time) sum += 1LL * (time - t[i]) * C;
         }
-        DEBUG(sum);
+        DEBUG("calWait: " << sum);
         return sum;
     };
 
     int l = 1, r = b.back();
     while (r - l > 2) {
         int mid1 = l + (r - l) / 3, mid2 = r - (r - l) / 3;
-        DEBUG(mid1 << '|' << mid2)
+        DEBUG(l << '|' << r)
+        DEBUG("cal: " << mid1);
         i64 c1 = calMove(mid1) + calWait(mid1);
+        DEBUG("cal: " << mid2);
         i64 c2 = calMove(mid2) + calWait(mid2);
-        if (c2 > c1) l = mid1;
-        else r = mid2;
+        if (c2 > c1) r = mid2;
+        else l = mid1;
     }
     DEBUG(l << '-' << r)
     i64 ans = LONG_LONG_MAX;
     for (int i = l; i <= r; i++) {
+        DEBUG("cal: " << i);
         ans = std::min(ans, calMove(i) + calWait(i));
     }
     std::cout << ans << std::endl;
