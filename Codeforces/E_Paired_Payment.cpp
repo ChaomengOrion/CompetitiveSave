0a43951334ff6502be9e251c2a238472f6599f59
ChaomengCFX <2198818239@qq.com>
2024-10-01 00:47:40 +0800

    .

--- Codeforces/E_Paired_Payment.cpp
+++ Codeforces/E_Paired_Payment.cpp
@@ -6,7 +6,7 @@ using i64 = long long;
 #define DEBUGV(vec, size) std::cerr << #vec << " = " << '['; for (int i = 0; i < size; i++) { std::cerr << vec[i]; if (i != size - 1) std::cerr << ", "; } std::cerr << ']' << std::endl;
 
 struct node {
-    std::vector<int> to;
+    std::vector<std::pair<int, int>> to;
     int cost = -1;
 };
 
@@ -16,14 +16,27 @@ void solve()
     std::cin >> n >> m;
     std::vector<node> graph(n + 1);
     for (int i = 0; i < m; i++) {
-        int u, v;
-        std::cin >> u >> v;
-        graph[u].to.push_back(v);
-        graph[v].to.push_back(u);
+        int u, v, c;
+        std::cin >> u >> v >> c;
+        graph[u].to.push_back({v, c});
+        graph[v].to.push_back({u, c});
     }
-    auto bfs = [&](auto&& bfs, int id) {
 
-    };
+    std::vector<std::array<int, 3>> wait; // p id pre_cost
+    bool flag = true;
+
+    wait.push_back({0, 1, 0});
+
+    while (!wait.empty()) {
+        std::vector<std::array<int, 3>> next;
+        for (auto [p, id, pc] : wait) {
+            for (auto [nxt, c] : graph[id].to) {
+                if (nxt != p) {
+                    next.push_back({id, nxt, pc + c});
+                }
+            }
+        }
+    }
 }
 
 int main()
