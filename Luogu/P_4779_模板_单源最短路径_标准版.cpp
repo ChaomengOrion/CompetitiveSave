b862df41b9d15897697af1aeef068115b10ef079
ChaomengCFX <2198818239@qq.com>
2024-10-23 12:15:18 +0800

    tr

--- Luogu/P_4779_模板_单源最短路径_标准版.cpp
+++ Luogu/P_4779_模板_单源最短路径_标准版.cpp
@@ -1,51 +1,36 @@
 #include <bits/stdc++.h>
 
-struct node {
-    std::vector<std::pair<int, int>> to;
-    bool finished = false;
-    int minDis = INT_MAX;
-};
-
-struct task {
-    int id, dis;
-    task(int id, int dis) : id(id), dis(dis) {}
-
-    bool operator>(const task& another) const {
-        return dis > another.dis;
-    }
-};
-
 void solve()
 {
     int N, M, S; // 点 边 出发点
     std::cin >> N >> M >> S;
-    std::vector<node> graph(N + 1);
+    std::vector<std::vector<std::pair<int, int>>> graph(N + 1);
     for (int i = 0; i < M; i++) {
         int u, v, len;
         std::cin >> u >> v >> len;
-        graph[u].to.emplace_back(v, len);
+        graph[u].emplace_back(v, len);
         //graph[v].to.emplace_back(u, len);
     }
 
-    std::priority_queue<task, std::vector<task>, std::greater<task>> pending;
-    graph[S].minDis = 0;
-    pending.emplace(S, 0);
-    while(!pending.empty()) {
-        node& cur = graph[pending.top().id];
-        pending.pop();
-        if (cur.finished) continue;
-        cur.finished = true;
-        for (auto [id, cost] : cur.to) {
-            if (graph[id].finished) continue;
-            if (graph[id].minDis > cur.minDis + cost) {
-                graph[id].minDis = cost + cur.minDis;
-                pending.emplace(id, cost + cur.minDis);
+    std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>, std::greater<>> pq;
+    std::vector<int> dis(N + 1, INT_MAX);
+    dis[S] = 0;
+    pq.emplace(0, S);
+    while(!pq.empty()) {
+        auto [d, i] = pq.top();
+        pq.pop();
+        if (d != dis[i]) continue;
+
+        for (auto [nid, cost] : graph[i]) {
+            if (dis[nid] > dis[i] + cost) {
+                dis[nid] = dis[i] + cost;
+                pq.emplace(dis[nid], nid);
             }
         }
     }
 
     for (int i = 1; i <= N; i++) {
-        std::cout << graph[i].minDis << ' ';
+        std::cout << dis[i] << ' ';
     }
 }
 
