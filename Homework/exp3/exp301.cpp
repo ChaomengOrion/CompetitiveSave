d06505c8f1eb111553a8234a199230f3d28adc60
ChaomengCFX <2198818239@qq.com>
2024-11-13 21:20:57 +0800

    .

---
+++ Homework/exp3/exp301.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using i64 = long long;
+
+#define LOG(...) std::cerr << "Line[" << __LINE__ << "]: " << __VA_ARGS__ << std::endl;
+#define LOGV(_vec, _size) std::cerr << #_vec << " = " << '['; for (int _i = 0; _i < (_size); _i++) { std::cerr << (_vec)[_i]; if (_i != (_size) - 1) std::cerr << ", "; } std::cerr << ']' << std::endl;
+
+void solve() {
+    int n;
+    std::cin >> n;
+    std::vector<std::string> strs(n);
+    for (int i = 0; i < n; i++) {
+        std::cin >> strs[i];
+    }
+    std::sort(strs.begin(), strs.end());
+    for (int i = 0; i < n; i++) {
+        std::cout << strs[i] << ' ';
+    }
+}
+
+int main() {
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+    solve();
+    return 0;
+}
--- Practice/test.cpp
+++ Practice/test.cpp
@@ -1,28 +1,43 @@
-#include <bits/stdc++.h>
 
-void test() {
-    return;
+
+#include <iostream>
+#include<cmath>
+using namespace std;
+int digit(int x)
+{
+    for (int k = 1; ; k++)
+    {
+        if (x / 10 == 0) return k;
+    }
 }
-
-void foo() {
-    return;
+bool num( int x )
+{
+    bool yes = 1;
+    int m = digit(x);
+    int* arr = new int [m];
+    for (int i = 0; i < m; i++)
+    {       
+        arr[i] = x % 10;
+        if(i<=m-2) x /= 10;
+    }
+    for (int k=0, j = m - 1 - k; k < j; k++)
+    {
+        if (arr[k] != arr[j])
+        {
+            yes = 0;
+            break;
+        }
+    }   
+    return yes;
 }
 
 int main()
 {
-    test();
-    (*test)();
-    (*&test)();
-    (&*test)();
-    (&test)();
-    (**********test)();
-    
-    
-    void (*a)()  = foo;
-    a();
-    (*a)();
-    (*&a)();
-    (&*a)();
-    //(&a)();
-    (**********a)();
+    int x;
+    while (cin >> x)
+    {
+        if (num(x) == 1) cout << "这是回文数" << endl;
+        else cout << "这不是回文数" << endl;
+    }
 }
+
